c791408c97a10cb83b7e44ca3216a3c4151f345f
AlexisLuna <alexisgabriel.lu@gmail.com>
2024-07-15 11:34:05 -0500

    Add sentencias de control in contrl-flow

--- src/app/shared/side-menu/side-menu.component.ts
+++ src/app/shared/side-menu/side-menu.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import DashboardComponent from '../../dashboard/dashboard.component';
+import { routes } from "../../app.routes"
+import { run } from 'node:test';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
+  public menuItems = routes
+    .map(route => route.children ?? [])
+    .flat()
+    .filter(route => route && route.path)
+    .filter(route => !route.path?.includes(":"))
+
+  constructor() {
+
+  }
 
 }
